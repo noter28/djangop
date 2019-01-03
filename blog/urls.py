00cff68033fd3873020c5d9f338d874e99345659
noter28 <noterfield@gmail.com>
2019-01-03 12:54:55 +0200

    CSS and JS

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', views.post_list, name='index'),
-    url(r'^1$', views.post_list, name='index'),
+    url(r'^ert/$', views.index, name='index'),
     url(r'^post/(?P<pk>\d+)$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
